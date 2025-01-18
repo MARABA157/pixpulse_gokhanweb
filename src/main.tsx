d46194d4e79d468b23416d311e175daa10156867
PixelPulse <pixelpulse@example.com>
2025-01-18 17:48:40 +0300

    Router: Move back to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
 import reportWebVitals from './utils/analytics';
@@ -16,11 +15,9 @@ if (import.meta.env.VITE_ENABLE_SENTRY === 'true') {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // Performance metrikleri
